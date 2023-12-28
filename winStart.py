53f276225ed12603959f3fa59068e4fac0e3f678
Nick1t0s <95224080+Nick1t0s@users.noreply.github.com>
2023-12-28 12:32:40 +0300

    Add files via upload

--- winStart.py
+++ winStart.py
@@ -1,13 +1,76 @@
-def downloadFlask():
-    os.system("pip3 install flask")
-def downloadLocalServ()
-import subprocess,time,os
+
+def downloadAndSetupMod(urlGit,name,fileName):
+    if not os.path.isdir(f"C:\\winTest\\{name}"):
+        installed=[]
+        noInstalled=[]
+        os.mkdir(f"C:\\winTest\\{name}")
+        try:
+            response = requests.get(urlGit)
+        except:
+            return "noReposytory"
+        zip_data = response.content
+        with open(f"C:\\winTest\\{name}\\archive.zip", 'wb') as file:
+            file.write(zip_data)
+        with ZipFile(f"C:\\winTest\\{name}\\archive.zip", 'r') as zip_ref:
+            zip_ref.extractall(f"C:\\winTest\\{name}")
+        os.remove(f"C:\\winTest\\{name}\\archive.zip")
+        dirSetupName=os.listdir(f"C:\\winTest\\{name}")[0]
+        shutil.move(f"C:\\winTest\\{name}\\{dirSetupName}\\{fileName}{'.py'}",f"C:\\winTest\\{name}\\{fileName}{'.pyw'}")
+        shutil.move(f"C:\\winTest\\{name}\\{dirSetupName}\\data",f"C:\\winTest\\{name}")
+        shutil.rmtree(f"C:\\winTest\\{name}\\{dirSetupName}")
+#        settingsFile=os.listdir(f"C;\\winTest\\{name}\\{dirSetupName}")[os.listdir(f"C;\\winTest\\{name}\\{dirSetupName}").index("modules.txt")]
+#        shutil.rmtree(f"C:\\winTest\\{name}")
+        print(dirSetupName)
+        modules=[]
+        with open(f"C:\\winTest\\{name}\\data\\modules.txt") as file:
+            for line in file:
+                modules.append(line.rstrip('\n'))
+        for i in modules:
+            if int(os.system(f"pip3 install {i}"))==0:
+                installed.append(i)
+            else:
+                noInstalled.append(i)
+        with open("C:\\winTest\\modules.txt") as file:
+            for line in file:
+                modules.append(line.rstrip('\n'))
+        modules=list(set(modules))
+        with open("C:\\winTest\\modules.txt","w") as file:
+            for i in modules:
+                file.write(i+'\n')
+        with open("C:\\winTest\\mods.txt","a") as file:
+            file.write(name)
+        return installed,noInstalled
+    else:
+        return "module installed"
+
+
+import subprocess,time,os,requests,shutil
+from zipfile import ZipFile
+if not os.path.isfile("C:\\winTest\\modules.txt"):
+    with open("C:\\winTest\\modules.txt","w"):
+        pass
+if not os.path.isfile("C:\\winTest\\mods.txt"):
+    with open("C:\\winTest\\mods.txt","w"):
+        pass
+mods=[]
+modules=[]
+with open("C:\\winTest\\mods.txt") as file:
+    for line in file:
+        mods.append(line.rstrip("\n"))
+for i in mods:
+    url,n1,n2=i.split(' ')
+    print(downloadAndSetupMod(url,n1,n2))
+
 
 # Запуск второго файла в отдельном процессе
-process = subprocess.Popen(['python', 'second_file.py'])
+#process = subprocess.Popen(['python', 'C:\\winTest\\localServ\\localServ.pyw'])
 
 # ждем некоторое время, прежде чем прервать выполнение
-time.sleep(5)
+#time.sleep(30)
 
 # Прерывание выполнения второго файла
-process.terminate()
+#process.terminate()
+
+
+#print(downloadAndSetupMod('https://api.github.com/repos/Nick1t0s/localServOnFlask/zipball','localServ','localServ'))
+print("dsffdsfg")
